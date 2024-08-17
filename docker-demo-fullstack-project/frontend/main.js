22f0bb193c7435d24de9a70383fbf630496f9409
YoussefHussien99 <51840033+youssefa111@users.noreply.github.com>
2024-08-17 23:02:47 +0300

    Add files via upload

---
+++ docker-demo-fullstack-project/frontend/main.js
@@ -0,0 +1,75 @@
+var idCounter = 3;
+const ipAddress = "http://13.60.166.183:8080"
+
+document.getElementById('studentForm').addEventListener('submit', async function(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    // Show the loading indicator
+    const loadingIndicator = document.getElementById('loadingIndicator');
+    loadingIndicator.style.display = 'block';
+
+    // Disable the submit button
+    const submitButton = document.getElementById('submitButton');
+    submitButton.disabled = true;
+
+    // Gather form data
+    const formData = {
+        id:idCounter++,
+        username: document.getElementById('username').value,
+        email: document.getElementById('email').value,
+        password: document.getElementById('password').value,
+        // age: document.getElementById('age').value,
+        // gender: document.getElementById('gender').value,
+        // favLanguage: document.getElementById('favLanguage').value
+    };
+
+    try {
+        await new Promise((resolve) => setTimeout(resolve, 3000));
+        // Send the data to the API
+        const response = await fetch(`${ipAddress}/user`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        });
+
+        // Handle the response
+        // const result = await response.json();
+        if (response.ok) {
+            alert('The student was added successfully!');
+        } else {
+            alert('An error occurred: ' + result.message);
+        }
+    } catch (error) {
+        alert('There was an error with the request: ' + error.message);
+    } finally {
+        // Hide the loading indicator
+        loadingIndicator.style.display = 'none';
+
+        // Re-enable the submit button
+        submitButton.disabled = false;
+    }
+});
+
+
+async function getAllUsers(){
+    try {
+        const response = await fetch(`${ipAddress}/user`, {
+            method:'GET',
+            headers:{
+                'Content-Type':'application/Json'
+            },
+        
+        })
+        const result = response.body;
+        if(response.ok){
+
+        }
+        else {
+            alert('An error occurred: ' + result.message);
+        }
+    } catch (error) {
+        alert('There was an error with the request: ' + error.message);
+    }
+}
